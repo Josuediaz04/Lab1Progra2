dc66a3296d277aa1abdf9f35066ab3a41edcace4
josue diaz <hernandzmarlon8@gmail.com>
2024-08-18 11:02:19 -0600

    imagen al formulario

--- CRoyale.Client/Form1.Designer.cs
+++ CRoyale.Client/Form1.Designer.cs
@@ -28,13 +28,15 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
 			this.SuspendLayout();
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(498, 563);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.Load += new System.EventHandler(this.Form1_Load);
