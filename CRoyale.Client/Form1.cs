e9e4c4de4233c315bf0e7c5f0de5881503272fb1
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2024-08-18 10:45:08 -0600

    cambios al formulario

--- CRoyale.Client/Form1.cs
+++ CRoyale.Client/Form1.cs
@@ -18,55 +18,57 @@ namespace CRoyale.Client
 			InitializeComponent();
 		}
 
+		private void TestCard() {
+            Card player1 = new Card();
+            player1.Name = "Golem";
+            Card player2 = new Card("Pekka");
+
+            player2.ScaleLevel(player2.Level + 5);
+            player1.ScaleLevel(player1.Level + 10);
+
+            ControlPlay engine = new ControlPlay();
+
+            for (int i = 0; i < 5; i++)
+            {
+                MessageBox.Show("Jugando! Turno del " + player1.Name);
+                engine.Attack(player1.Damage, player2);
+                if (engine.ValidateHP(player2.HP))
+                {
+                    MessageBox.Show(player2.Name + " PERDIO");
+                    MessageBox.Show($"{player2.Name}. {player2.HP} ");
+                    engine.GainXP(player1);
+                    MessageBox.Show(player1.Name + " GANA y Obtiene mas experiencia: " + player1.XP);
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show($"{player2.Name}. {player2.HP} - {player2.Damage} - {player2.Shield} ");
+                }
+
+                MessageBox.Show("Jugando! Turno del " + player2.Name);
+                engine.Attack(player2.Damage, player1);
+                if (engine.ValidateHP(player1.HP))
+                {
+                    MessageBox.Show(player1.Name + " PERDIO");
+                    MessageBox.Show($"{player1.Name}. {player1.HP}");
+                    engine.GainXP(player2);
+                    MessageBox.Show(player2.Name + " GANA y Obtiene mas experiencia: " + player2.XP);
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show($"{player1.Name}. {player1.HP} - {player1.Damage} - {player1.Shield} ");
+                }
+
+
+
+            }
+
+        }
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			Card player1 = new Card();
-			player1.Name = "Golem";
-			Card player2 = new Card("Pekka");
+            //TestCard();
 
-			player2.ScaleLevel(player2.Level + 5);
-			player1.ScaleLevel(player1.Level + 10);
-
-			ControlPlay engine = new ControlPlay();
-
-			for (int i = 0; i < 5; i++)
-			{
-				MessageBox.Show("Jugando! Turno del " + player1.Name);
-				engine.Attack(player1.Damage, player2);
-				if (engine.ValidateHP(player2.HP))
-				{
-					MessageBox.Show(player2.Name + " PERDIO");
-					MessageBox.Show($"{player2.Name}. {player2.HP} ");
-					engine.GainXP(player1);
-					MessageBox.Show(player1.Name + " GANA y Obtiene mas experiencia: " + player1.XP);
-					return;
-				}
-				else
-				{
-					MessageBox.Show($"{player2.Name}. {player2.HP} - {player2.Damage} - {player2.Shield} ");
-				}
-
-				MessageBox.Show("Jugando! Turno del " + player2.Name);
-				engine.Attack(player2.Damage, player1);
-				if (engine.ValidateHP(player1.HP))
-				{
-					MessageBox.Show(player1.Name + " PERDIO");
-					MessageBox.Show($"{player1.Name}. {player1.HP}");
-					engine.GainXP(player2);
-					MessageBox.Show(player2.Name + " GANA y Obtiene mas experiencia: " + player2.XP);
-					return;
-				}
-				else
-				{
-					MessageBox.Show($"{player1.Name}. {player1.HP} - {player1.Damage} - {player1.Shield} ");
-				}
-
-
-
-			}
-
-
-
-		}
+        }
 	}
 }
